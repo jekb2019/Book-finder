f1f80287d5eb73275e161c62ed6e7699c27fa637
jekb2019 <jekb2019@gmail.com>
2021-08-03 21:52:11 +1200

    Set up Frontend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import BookList from './components/BookList';
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Jason's Reading List</h1>
+      <BookList/>
+    </div>
+  );
+}
+
+export default App;
