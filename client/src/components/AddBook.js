8ccdf48324281f4389b1d28b2912f2aeb1ca5244
jekb2019 <jekb2019@gmail.com>
2021-08-05 03:25:53 +1200

    Implement add new book

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,34 +1,44 @@
-import React from 'react'
-import { useQuery } from "@apollo/client";
-import { getAuthors } from '../queries/queries';
+import React, { useState } from 'react'
+import { useMutation, useQuery } from "@apollo/client";
+import { getAuthors, addBook, getBooks } from '../queries/queries';
 
 function AddBook() {
-    const {loading, error, data} = useQuery(getAuthors);
+    const {loading: getAuthorLoading, error: getAuthorError, data: getAuthorData} = useQuery(getAuthors);
+    const [addBookMutateFunction, { data: addBookData, loading: addBookLoading, error: addBookError }] = useMutation(addBook);
+
+    const [name, setName] = useState("");
+    const [genre, setGenre] = useState("");
+    const [authorId, setAutherId] = useState("");
 
     const displayAuthors = () => {
-        if(loading) return <option> Loading Authors... </option>;
-        if(error) return <option> Error loading authors...</option>;
-        if(data) {
-            const authors = data.authors;
+        if(getAuthorLoading) return <option> Loading Authors... </option>;
+        if(getAuthorError) return <option> Error loading authors...</option>;
+        if(getAuthorData) {
+            const authors = getAuthorData.authors;
             return authors.map((author) => {
                 return <option key={author.id} value={author.id}>{author.name}</option>
             })
         }
     }
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log(name, genre, authorId);;
+        a
+    }
 
     return (
-        <form id="add-book">
+        <form id="add-book" onSubmit={submitForm}>
             <div className="field">
                 <label>Book name:</label>
-                <input type="text"/>
+                <input onChange={(e) => setName(e.target.value)} type="text"/>
             </div>
             <div className="field">
                 <label>Genre:</label>
-                <input type="text"/>
+                <input onChange={(e) => setGenre(e.target.value)} type="text"/>
             </div>
             <div className="field">
                 <label>Author</label>
-                <select>
+                <select onChange={(e) => setAutherId(e.target.value)}>
                     <option>Select author</option>
                     {displayAuthors()}
                 </select>
